2360f0c2f9b7ec0cb78281eee20dabb9d4f07b36
tan <t.songglod@gmail.com>
2022-02-08 11:52:44 +0700

    [+] add week 4

---
+++ week-4/basic-dynamic-style/README.md
@@ -0,0 +1,3 @@
+# Week 4
+
+- lecture & lab 4
---
+++ week-4/basic-dynamic-style/src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+const invalidMessage = "red";
+const italicText = "italic";
+const blueText = "color: blue";
+const greyText = "color: yellow";
+const italicStyle = "font-style: italic";
+</script>
+
+<template>
+	<input type="text" />
+	<p :style="{ color: invalidMessage, 'font-style': italicText }">
+		This is a text
+	</p>
+	<p :style="{ color: true ? 'green' : invalidMessage }">Red or Green</p>
+	<p :style="[italicStyle, false ? greyText : blueText]">
+		asdasdasdlsdjnlsndv nsdjnvkjekuve
+	</p>
+	<p :class="['textUnderline', true ? 'normalMsg' : 'errorMsg']">
+		nsdjnvkjekuve
+	</p>
+	<h1 class="text-3xl font-bold underline">Hello, Tailwind!</h1>
+</template>
+
+<style>
+.errorMsg {
+	color: red;
+}
+
+.normalMsg {
+	color: rgb(177, 177, 177);
+}
+
+.textUnderline {
+	text-decoration: underline;
+}
+</style>
